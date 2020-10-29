0c5c4d32146e3a3b6b584206c1cdf7b296e36bcc
40012435 <dr-ayeayemyint@infomyanmarcollege.com>
2020-10-29 22:00:39 +0630

    displayEmployee method added

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -69,6 +69,20 @@ public class App
             }
         }
     }
+    public void displayEmployee(Employee emp)
+    {
+        if (emp != null)
+        {
+            System.out.println(
+                    emp.emp_no + " "
+                            + emp.first_name + " "
+                            + emp.last_name + "\n"
+                            + emp.title + "\n"
+                            + "Salary:" + emp.salary + "\n"
+                            + emp.dept_name + "\n"
+                            + "Manager: " + emp.manager + "\n");
+        }
+    }
     public Employee getEmployee(int ID)
     {
         try
