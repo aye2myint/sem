893f108acb29b35c4a5f13e2a0a9eb53c903b4b9
40012435 <dr-ayeayemyint@infomyanmarcollege.com>
2020-10-30 21:18:08 +0630

    App changed in main .display method

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -118,10 +118,15 @@ public class App
     }
     public static void main(String[] args) {
 // Create new Application
+        // Create new Application
         App a = new App();
 
         // Connect to database
         a.connect();
+        // Get Employee
+        Employee emp = a.getEmployee(255530);
+        // Display results
+        a.displayEmployee(emp);
 
         // Disconnect from database
         a.disconnect();
